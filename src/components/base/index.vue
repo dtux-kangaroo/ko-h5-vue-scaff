82f12ffbf7c713b8cfb5c9020e8fed27e4418921
ziyang <ziyang@dtstack.com>
2020-12-10 19:33:09 +0800

    build:配置路由

---
+++ src/components/base/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <van-tabbar v-model="active">
+    <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="search">标签</van-tabbar-item>
+    <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+    <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+  </van-tabbar>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Tabbar, TabbarItem } from "vant";
+export default defineComponent({
+  name: "App",
+  components: {
+    Tabbar,
+    TabbarItem,
+  },
+  computed: {
+    showFooter() {
+      return this.$route.path.split("/").length < 3;
+    },
+  },
+});
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -10,13 +10,11 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../pages/About.vue')
+    component: () => import('@/pages/About.vue')
   }
 ]
 
+//使用createRouter 来创建router实例
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
